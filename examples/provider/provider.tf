526d049dc5534878966369be3dfbe0981ef6478f
Jeremy Mouton <jeremy.mouton@spendesk.com>
2024-10-31 22:24:55 +0100

    fmt

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,5 +2,5 @@ provider "metabase" {
   endpoint = "http://your-metabase-instance.com/api"
   username = "your-metabase-admin-username" # or use api_key
   password = "your-metabase-admin-password" # or use api_key
-  api_key = "your-metabase-admin-api-key" # or use username/password
+  api_key  = "your-metabase-admin-api-key"  # or use username/password
 }
--- examples/resources/metabase_permissions_membership.tf
+++ examples/resources/metabase_permissions_membership.tf
@@ -1,5 +1,5 @@
 resource "metabase_permissions_membership" "example2" {
-  group_id = metabase_permissions_group.example.id
-  user_id  = metabase_user.example.id
+  group_id         = metabase_permissions_group.example.id
+  user_id          = metabase_user.example.id
   is_group_manager = false # can't be changed after creation if you don't have a Premium license
 }
