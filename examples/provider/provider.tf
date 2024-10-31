56ed2ad7f7efedfc8d568b3f9435cd98557be7c2
Jeremy Mouton <jeremy.mouton@spendesk.com>
2024-10-31 18:10:07 +0100

    Update examples

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,6 @@
-provider "scaffolding" {
-  # example configuration here
+provider "metabase" {
+  endpoint = "http://your-metabase-instance.com/api"
+  username = "your-metabase-admin-username" # or use api_key
+  password = "your-metabase-admin-password" # or use api_key
+  api_key = "your-metabase-admin-api-key" # or use username/password
 }
---
+++ examples/resources/metabase_user.tf
@@ -0,0 +1,5 @@
+resource "metabase_user" "example" {
+  email      = "example@example.fr"
+  first_name = "example"
+  last_name  = "example"
+}
