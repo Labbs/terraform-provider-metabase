dc2025b325ac3e63e62ba0dd4c09057ccacfc853
Jeremy Mouton <jeremy.mouton@spendesk.com>
2024-11-08 16:15:01 +0100

    Feat(database): Add functions for PostgreSQL details attribute handling and type verification

--- metabase/utils.go
+++ metabase/utils.go
@@ -46,3 +46,71 @@ func TransformPostgresDetails(details PostgresqlDetails) map[string]attr.Value {
 
 	return result
 }
+
+func detailsPostgresAttribute(attrMap map[string]attr.Value) map[string]interface{} {
+	details := make(map[string]interface{})
+
+	if host, ok := attrMap["host"].(types.String); ok {
+		details["host"] = host.ValueString()
+	}
+	if port, ok := attrMap["port"].(types.Int64); ok {
+		details["port"] = port.ValueInt64()
+	}
+	if db, ok := attrMap["database"].(types.String); ok {
+		details["db"] = db.ValueString()
+	}
+	if user, ok := attrMap["user"].(types.String); ok {
+		details["user"] = user.ValueString()
+	}
+	if password, ok := attrMap["password"].(types.String); ok {
+		details["password"] = password.ValueString()
+	}
+	if schemaFilter, ok := attrMap["schema_filter"].(types.String); ok {
+		details["schema-filter"] = schemaFilter.ValueString()
+	}
+	if ssl, ok := attrMap["ssl"].(types.Bool); ok {
+		details["ssl"] = ssl.ValueBool()
+	}
+	if sslMode, ok := attrMap["ssl_mode"].(types.String); ok {
+		details["ssl-mode"] = sslMode.ValueString()
+	}
+	if sslUseClientMode, ok := attrMap["ssl_use_client_mode"].(types.Bool); ok {
+		details["ssl-use-client-mode"] = sslUseClientMode.ValueBool()
+	}
+
+	return details
+}
+
+func PostgresqlDetailsVerifyType(details map[string]interface{}) PostgresqlDetails {
+	var result PostgresqlDetails
+
+	if host, ok := details["host"].(string); ok {
+		result.Host = types.StringValue(host)
+	}
+	if port, ok := details["port"].(int64); ok {
+		result.Port = types.Int64Value(port)
+	}
+	if db, ok := details["db"].(string); ok {
+		result.Database = types.StringValue(db)
+	}
+	if user, ok := details["user"].(string); ok {
+		result.User = types.StringValue(user)
+	}
+	if password, ok := details["password"].(string); ok {
+		result.Password = types.StringValue(password)
+	}
+	if schemaFilter, ok := details["schema-filter"].(string); ok {
+		result.SchemaFilter = types.StringValue(schemaFilter)
+	}
+	if ssl, ok := details["ssl"].(bool); ok {
+		result.SSL = types.BoolValue(ssl)
+	}
+	if sslMode, ok := details["ssl-mode"].(string); ok {
+		result.SSLMode = types.StringValue(sslMode)
+	}
+	if sslUseClientMode, ok := details["ssl-use-client-mode"].(bool); ok {
+		result.SSLUseClientMode = types.BoolValue(sslUseClientMode)
+	}
+
+	return result
+}
