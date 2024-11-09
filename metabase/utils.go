7d20141218569c9f1b9325722b67034a8643b79e
Jeremy Mouton <jeremy.mouton@spendesk.com>
2024-11-09 03:36:05 +0100

    feat(database): Implement MySQL details transformation and verification functions

--- metabase/utils.go
+++ metabase/utils.go
@@ -47,6 +47,25 @@ func TransformPostgresDetails(details PostgresqlDetails) map[string]attr.Value {
 	return result
 }
 
+func TransformMysqlDetails(details MysqlDetails) map[string]attr.Value {
+	var result = make(map[string]attr.Value)
+	result["host"] = types.StringValue(details.Host.ValueString())
+
+	if details.Port.ValueInt64() == 0 {
+		result["port"] = types.Int64Value(3306)
+	} else {
+		result["port"] = types.Int64Value(details.Port.ValueInt64())
+	}
+
+	result["database"] = types.StringValue(details.Database.ValueString())
+
+	result["user"] = types.StringValue(details.User.ValueString())
+
+	result["password"] = types.StringValue(details.Password.ValueString())
+
+	return result
+}
+
 func detailsPostgresAttribute(attrMap map[string]attr.Value) map[string]interface{} {
 	details := make(map[string]interface{})
 
@@ -81,6 +100,28 @@ func detailsPostgresAttribute(attrMap map[string]attr.Value) map[string]interfac
 	return details
 }
 
+func detailsMysqlAttribute(attrMap map[string]attr.Value) map[string]interface{} {
+	details := make(map[string]interface{})
+
+	if host, ok := attrMap["host"].(types.String); ok {
+		details["host"] = host.ValueString()
+	}
+	if port, ok := attrMap["port"].(types.Int64); ok {
+		details["port"] = port.ValueInt64()
+	}
+	if db, ok := attrMap["database"].(types.String); ok {
+		details["db"] = db.ValueString()
+	}
+	if user, ok := attrMap["user"].(types.String); ok {
+		details["user"] = user.ValueString()
+	}
+	if password, ok := attrMap["password"].(types.String); ok {
+		details["password"] = password.ValueString()
+	}
+
+	return details
+}
+
 func PostgresqlDetailsVerifyType(details map[string]interface{}) PostgresqlDetails {
 	var result PostgresqlDetails
 
@@ -110,6 +151,28 @@ func PostgresqlDetailsVerifyType(details map[string]interface{}) PostgresqlDetai
 	}
 	if sslUseClientMode, ok := details["ssl-use-client-mode"].(bool); ok {
 		result.SSLUseClientMode = types.BoolValue(sslUseClientMode)
+	}
+
+	return result
+}
+
+func MysqlDetailsVerifyType(details map[string]interface{}) MysqlDetails {
+	var result MysqlDetails
+
+	if host, ok := details["host"].(string); ok {
+		result.Host = types.StringValue(host)
+	}
+	if port, ok := details["port"].(int64); ok {
+		result.Port = types.Int64Value(port)
+	}
+	if db, ok := details["db"].(string); ok {
+		result.Database = types.StringValue(db)
+	}
+	if user, ok := details["user"].(string); ok {
+		result.User = types.StringValue(user)
+	}
+	if password, ok := details["password"].(string); ok {
+		result.Password = types.StringValue(password)
 	}
 
 	return result
